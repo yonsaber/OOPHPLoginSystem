dfe37fb861362ca48017e201b133505d6f4d8eb0
yonsaber <matt@mattredmond.co.uk>
2013-12-27 22:40:12 +0000

    Revert "Episode 8 -- Working on Database functions. Select and Delete created. Config.php, allows for sections of the config to be fetched (Shown in Database.php - Line 12)."
    This reverts commit 9dc6381b8ca7e47c68e539fdc1c613cdb3779b4f.

--- core/initPublic.php
+++ core/initPublic.php
@@ -15,10 +15,4 @@
 			'sessionName'	=> 'user'
 		)
 	);
-	
-	spl_autoload_register(function($class) {
-		require_once 'classes/'.$class.'.php';
-	});
-
-	require_once 'functions/sanitize.func.php';
 ?>
